fe2533263165c111178227a876cc6a40fd58317a
Pedro Fernandes <heeypedroo@gmai.com>
2022-10-23 14:29:38 -0300

    so falta versionamento

--- script.js
+++ script.js
@@ -4,46 +4,48 @@ while(qtd<4 || qtd>14 ||qtd%2!==0){
     qtd = prompt('Com quantas cartas quer jogar?');
 }
 
-const arrayCompleto = [`<div onclick="virar(this)" class="card">
+
+
+const arrayCompleto = [`<div onclick="virar(this)" class="card metal">
 <div class="back face"><img src="arquivos/metalparrot.gif"></div>
 <div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
-</div>`, `<div onclick="virar(this)" class="card">
+</div>`, `<div onclick="virar(this)" class="card metal">
 <div class="back face"><img src="arquivos/metalparrot.gif"></div>
 <div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
-</div>`, `<div onclick="virar(this)" class="card">
+</div>`, `<div onclick="virar(this)" class="card bobros">
 <div class="back face"><img src="arquivos/bobrossparrot.gif"></div>
 <div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
-</div>`, `<div onclick="virar(this)" class="card">
+</div>`, `<div onclick="virar(this)" class="card bobros">
 <div class="back face"><img src="arquivos/bobrossparrot.gif"></div>
 <div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
-</div>`, `<div onclick="virar(this)" class="card">
+</div>`, `<div onclick="virar(this)" class="card explody">
 <div class="back face"><img src="arquivos/explodyparrot.gif"></div>
 <div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
-</div>`, `<div onclick="virar(this)" class="card">
+</div>`, `<div onclick="virar(this)" class="card explody">
 <div class="back face"><img src="arquivos/explodyparrot.gif"></div>
 <div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
-</div>`, `<div onclick="virar(this)" class="card">
+</div>`, `<div onclick="virar(this)" class="card fiesta">
 <div class="back face"><img src="arquivos/fiestaparrot.gif"></div>
 <div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
-</div>`, `<div onclick="virar(this)" class="card">
+</div>`, `<div onclick="virar(this)" class="card fiesta">
 <div class="back face"><img src="arquivos/fiestaparrot.gif"></div>
 <div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
-</div>`, `<div onclick="virar(this)" class="card">
+</div>`, `<div onclick="virar(this)" class="card revert">
 <div class="back face"><img src="arquivos/revertitparrot.gif"></div>
 <div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
-</div>`, `<div onclick="virar(this)" class="card">
+</div>`, `<div onclick="virar(this)" class="card revert">
 <div class="back face"><img src="arquivos/revertitparrot.gif"></div>
 <div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
-</div>`, `<div onclick="virar(this)" class="card">
+</div>`, `<div onclick="virar(this)" class="card triple">
 <div class="back face"><img src="arquivos/tripletsparrot.gif"></div>
 <div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
-</div>`, `<div onclick="virar(this)" class="card">
+</div>`, `<div onclick="virar(this)" class="card triple">
 <div class="back face"><img src="arquivos/tripletsparrot.gif"></div>
 <div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
-</div>`, `<div onclick="virar(this)" class="card">
+</div>`, `<div onclick="virar(this)" class="card uni">
 <div class="back face"><img src="arquivos/unicornparrot.gif"></div>
 <div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
-</div>`, `<div onclick="virar(this)" class="card">
+</div>`, `<div onclick="virar(this)" class="card uni">
 <div class="back face"><img src="arquivos/unicornparrot.gif"></div>
 <div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
 </div>`]
@@ -63,11 +65,58 @@ for(let j =0; j<qtd;j++){
 }
 
 
+let virouCarta = false;
+let primeiraCarta;
+let segundaCarta;
+let costa;
+let frente;
+let scosta;
+let sfrente;
 
+function desvirar(){
+    costa.classList.remove('back-flip')
+    frente.classList.remove('front-flip')
+    scosta.classList.remove('back-flip')
+    sfrente.classList.remove('front-flip')
+}
+let jogadas = 0;
+let acertos = 0;
 
 function virar(carta){
-    const costa = carta.querySelector('.back')
-    const frente = carta.querySelector('.front')
-    costa.classList.add('back-flip')
-    frente.classList.add('front-flip')
+    
+    jogadas++;
+
+    if(virouCarta===false){
+
+        costa = carta.querySelector('.back')
+        frente = carta.querySelector('.front')
+        costa.classList.add('back-flip')
+        frente.classList.add('front-flip')
+
+        virouCarta = true;
+        primeiraCarta = carta;
+    } else{
+
+        scosta = carta.querySelector('.back')
+        sfrente = carta.querySelector('.front')
+        scosta.classList.add('back-flip')
+        sfrente.classList.add('front-flip')
+
+        virouCarta = false;
+        segundaCarta = carta;
+    
+        if(primeiraCarta.classList.value === segundaCarta.classList.value){
+            
+            primeiraCarta.onclick = null;
+            segundaCarta.onclick = null;
+            acertos +=2;
+        } else{
+            setTimeout(desvirar, 1000);
+        }
+        if(qtd == acertos){
+            alert(`Você ganhou em ${jogadas} jogadas!`)
+        }
+       
+    }
+    
 }

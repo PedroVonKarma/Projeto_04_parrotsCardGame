18917e7bd60f9a9380995dcdca4c3ce25c586c4a
Pedro Fernandes <heeypedroo@gmai.com>
2022-10-22 23:33:09 -0300

    cartas virando corretamente

--- script.js
+++ script.js
@@ -1,20 +1,73 @@
-/*let qtd = prompt('Com quantas cartas quer jogar?');
+let qtd = prompt('Com quantas cartas quer jogar?');
 while(qtd<4 || qtd>14 ||qtd%2!==0){
     alert('Quantidade inválida. O número de cartas precisar ser par, entre 4 e 14!')
     qtd = prompt('Com quantas cartas quer jogar?');
 }
-const listaDeCartas = document.querySelector('.cards');
+
+const arrayCompleto = [`<div onclick="virar(this)" class="card">
+<div class="back face"><img src="arquivos/metalparrot.gif"></div>
+<div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
+</div>`, `<div onclick="virar(this)" class="card">
+<div class="back face"><img src="arquivos/metalparrot.gif"></div>
+<div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
+</div>`, `<div onclick="virar(this)" class="card">
+<div class="back face"><img src="arquivos/bobrossparrot.gif"></div>
+<div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
+</div>`, `<div onclick="virar(this)" class="card">
+<div class="back face"><img src="arquivos/bobrossparrot.gif"></div>
+<div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
+</div>`, `<div onclick="virar(this)" class="card">
+<div class="back face"><img src="arquivos/explodyparrot.gif"></div>
+<div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
+</div>`, `<div onclick="virar(this)" class="card">
+<div class="back face"><img src="arquivos/explodyparrot.gif"></div>
+<div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
+</div>`, `<div onclick="virar(this)" class="card">
+<div class="back face"><img src="arquivos/fiestaparrot.gif"></div>
+<div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
+</div>`, `<div onclick="virar(this)" class="card">
+<div class="back face"><img src="arquivos/fiestaparrot.gif"></div>
+<div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
+</div>`, `<div onclick="virar(this)" class="card">
+<div class="back face"><img src="arquivos/revertitparrot.gif"></div>
+<div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
+</div>`, `<div onclick="virar(this)" class="card">
+<div class="back face"><img src="arquivos/revertitparrot.gif"></div>
+<div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
+</div>`, `<div onclick="virar(this)" class="card">
+<div class="back face"><img src="arquivos/tripletsparrot.gif"></div>
+<div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
+</div>`, `<div onclick="virar(this)" class="card">
+<div class="back face"><img src="arquivos/tripletsparrot.gif"></div>
+<div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
+</div>`, `<div onclick="virar(this)" class="card">
+<div class="back face"><img src="arquivos/unicornparrot.gif"></div>
+<div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
+</div>`, `<div onclick="virar(this)" class="card">
+<div class="back face"><img src="arquivos/unicornparrot.gif"></div>
+<div class="front face"><img src="arquivos/back.png" alt="pagaio"></div>
+</div>`]
+let arrayUsavel =[];
+
 for(let i =0; i<qtd;i++){
-    listaDeCartas.innerHTML += ``
+    arrayUsavel.push(arrayCompleto[i]);    
+}
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+arrayUsavel.sort(comparador);
+
+const nomeA = document.querySelector('.cards');
+for(let j =0; j<qtd;j++){
+   nomeA.innerHTML += arrayUsavel[j];    
 }
 
-/*function virarCarta(cartita){
-    cartita.classList.add('.virada')
-    cartita
-}*/
 
-const cartas = document.querySelectorAll('.card');
-function flipar(){
-    this.classList.add('flip')
+
+
+function virar(carta){
+    const costa = carta.querySelector('.back')
+    const frente = carta.querySelector('.front')
+    costa.classList.add('back-flip')
+    frente.classList.add('front-flip')
 }
-cartas.forEach(carta => carta.addEventListener('click', flipar))
